1e4133e68c406b9560fb016a1ecfdfe5a87970d5
Shivtosh705 <117893149+Shivtosh705@users.noreply.github.com>
2023-04-08 18:09:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ const person = {
 const team = players;
 
 // Create a variable "team1" that is a copy of the "players" array using the spread operator
-const team1 = [players];
+const team1 = ["John", "Bob", "Alice", "Poppy"];
 
 // Create a variable "cap1" that is a copy of the "person" object using the spread operator
-const cap1 = {person};
+const cap1 = {"John Doe"};
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 window.players = players;
